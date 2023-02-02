03e50a0813bc98627de9b8569003da8b5f2d4b80
Pavlina <pavlina.sc@gmail.com>
2023-02-02 20:53:52 +0100

    nove subory

--- uloha1.py
+++ uloha1.py
@@ -2,3 +2,16 @@ jmeno = 'Zlatoslava'
 domena = 'czechitas.cz'
 
 print(jmeno+"@"+domena)
+
+jmeno_a_prijmeni= 'Jana Mala'
+#jmeno_a_prijmeni= 'Jana'+' '+'Mala'
+print(jmeno_a_prijmeni.upper())
+print(jmeno_a_prijmeni.lower())
+print(jmeno_a_prijmeni.capitalize())
+#print(jmeno_a_prijmeni.lower[0])
+inicial1=(jmeno_a_prijmeni[0:1])
+inicial2=(jmeno_a_prijmeni[5:6])
+print(inicial1+'.'+" "+ inicial2+'.')
+if jmeno >=[6]:
+    jmeno = jmeno[0:1]
+else: print(jmeno_a_prijmeni)
